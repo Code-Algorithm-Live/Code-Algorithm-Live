3b5ecb2f8c808d34cde677f901b0f5d8d5d33d85
차우열 <dtdtdz@gmail.com>
2024-01-17 14:51:17 +0900

    feat: problem controller 더미 생성

--- backend/coala/src/main/java/com/ssafy/coala/domain/problem/domain/Problem.java
+++ backend/coala/src/main/java/com/ssafy/coala/domain/problem/domain/Problem.java
@@ -1,13 +1,21 @@
 package com.ssafy.coala.domain.problem.domain;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 //@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Problem {
     int id;
     String title;
-    int acceptedUserCount;
+    int accepted_user_count;
     int level;
-    boolean giveNoRating;
-    float averageTries;
+    boolean give_no_rating;
+    float average_tries;
 }
