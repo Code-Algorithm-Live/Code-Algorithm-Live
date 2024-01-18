e5617813c7b5d948669fb90e1c97a4826f63c254
유현정 <s98415@naver.com>
2024-01-18 14:05:28 +0900

    Feat: Add setting files

---
+++ frontend/next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // ... 생략
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { IconNext } from "@assets/svgs";
+import "@/styles/reset.css";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <IconNext width={120} height={32} />
+    </main>
+  );
+}
