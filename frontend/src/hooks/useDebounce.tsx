470529fe514cfd2e760dfaf93f7689998ea9908d
박진아 <dndbekfrl1@gmail.com>
2024-01-19 13:56:59 +0900

    React query 설정

---
+++ frontend/src/hooks/useDebounce.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+function useDebounce<T>(value: T, delay: number): T {
+  // 커밋 테스트 3
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+export default useDebounce;
