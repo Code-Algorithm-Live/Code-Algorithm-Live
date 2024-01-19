11879b94ebf4ad3218490a8c0c1e6cea275a6792
dev-hamster <hamsterwave0925@gmail.com>
2024-01-19 16:31:16 +0900

    Style: 코드 스타일

--- frontend/src/utils/Provider.tsx
+++ frontend/src/utils/Provider.tsx
@@ -1,16 +1,13 @@
-"use client"
+'use client';
 
-import {
-    QueryClient,
-    QueryClientProvider,
-} from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
-const Provider = ({children}:{children:React.ReactNode}) =>{
-    return (
-        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    )
-}
+const Provider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
 
-export default Provider
+export default Provider;
