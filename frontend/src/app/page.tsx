138e1d7607e5bb22ec011b153c8703cbafe9307e
이재종 <wowhd45@naver.com>
2024-01-19 13:50:28 +0900

    test3 S10P12A709-59

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,7 @@ import { IconNext } from "@assets/svgs";
 import "@/styles/reset.css";
 
 export default function Home() {
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <IconNext width={120} height={32} />
