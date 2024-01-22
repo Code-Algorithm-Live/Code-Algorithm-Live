f083a77736e58d8e5b9b665f480f1829d756f991
이재종 <wowhd45@naver.com>
2024-01-22 13:29:36 +0900

    Feat: 헤더 구현, 폰트 추가 S10P12A709-122

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
-import './globals.css';
+import '@/styles/globals.css';
 import Provider from '@/utils/Provider';
+import Nav from '@/components/Common/Header';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Nav />
         <Provider>{children}</Provider>
       </body>
     </html>
