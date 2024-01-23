64bf0dd913aefa011a50fd902994a9f9bef90280
jaejin <ghkfuddks@naver.com>
2024-01-23 17:07:39 +0900

    feature : S10P12A709-130

---
+++ backend/coala/src/main/java/com/ssafy/coala/domain/help/service/MatchingServiceImpl.java
@@ -0,0 +1,33 @@
+package com.ssafy.coala.domain.help.service;
+
+import com.ssafy.coala.domain.member.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MatchingServiceImpl implements MatchingService{
+
+    @Autowired
+    private RedisService redisService;
+
+    @Override
+    public void performMatching() {
+        List<Object> waitingUsers = redisService.getAllUsers();
+
+        // Implement your matching logic here based on waitingUsers
+        // ...
+
+        // Example: Pair the first two users in the queue
+        if (waitingUsers.size() >= 2) {
+//            Member user1 = waitingUsers.get(0);
+//            Member user2 = waitingUsers.get(1);
+
+            // Perform matching logic here...
+
+            // Remove matched users from the queue
+
+        }
+    }
+}
