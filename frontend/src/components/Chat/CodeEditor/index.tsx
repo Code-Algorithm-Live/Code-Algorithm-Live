7f5190f6d3c5c9c0da11dbe7df759bd693e097c3
dev-hamster <hamsterwave0925@gmail.com>
2024-02-05 14:29:07 +0900

    Feat: 공동 편집 S10P12A709-147

--- frontend/src/components/Chat/CodeEditor/index.tsx
+++ frontend/src/components/Chat/CodeEditor/index.tsx
@@ -4,12 +4,13 @@ import { java } from '@codemirror/lang-java';
 import { atomone } from '@uiw/codemirror-theme-atomone';
 import ReactCodeMirror, {
   EditorView,
+  ReactCodeMirrorRef,
   Transaction,
   ViewUpdate,
 } from '@uiw/react-codemirror';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
-import yorkie, { DocEventType, OperationInfo } from 'yorkie-js-sdk';
+import yorkie, { DocEventType, EditOpInfo, OperationInfo } from 'yorkie-js-sdk';
 import { YorkieDoc } from './type';
 
 const Container = styled.div`
@@ -41,66 +42,115 @@ const DOC_NAME = `hamster-${new Date()
 
 const CodeEditor = () => {
   const ref = useRef<HTMLDivElement>(null);
-  const [maxHeight, setMaxHeight] = useState('');
-  const doc = useRef(new yorkie.Document<YorkieDoc>(DOC_NAME));
-  const [content, setContent] = useState<
-    {
-      from: number;
-      to: number;
-      insert: any;
-    }[]
-  >();
+  const codeMirrorView = useRef<ReactCodeMirrorRef>({});
   const client = useRef(
     new yorkie.Client(yorkieBaseURL, {
       apiKey: YORKIE_API_KEY,
     }),
   );
+  const [doc] = useState(new yorkie.Document<YorkieDoc>(DOC_NAME)); // useRef(new yorkie.Document<YorkieDoc>(DOC_NAME));
+  const [content, setContent] = useState('');
+  const [maxHeight, setMaxHeight] = useState('');
 
-  function handleEditOp(op: unknown) {
+  const handleEditOp = (op: EditOpInfo) => {
     const changes = [
       {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
         from: Math.max(0, op.from),
         to: Math.max(0, op.to),
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        insert: op.value!.content,
+        insert: op.value.content,
       },
     ];
 
-    setContent(changes);
-  }
+    codeMirrorView.current.view?.dispatch({
+      changes,
+      annotations: [Transaction.remote.of(true)],
+    });
+  };
 
-  function handleOperations(operations: Array<OperationInfo>) {
+  const handleOperations = (operations: OperationInfo[]) => {
     operations.forEach(op => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       if (op.type === 'edit') {
         handleEditOp(op);
       }
     });
-  }
+  };
 
-  const handleChange = useCallback((value: string, viewUpdate: ViewUpdate) => {
-    if (viewUpdate.docChanged) {
-      // eslint-disable-next-line no-restricted-syntax
-      for (const tr of viewUpdate.transactions) {
-        const events = ['select', 'input', 'delete', 'move', 'undo', 'redo'];
-        if (!events.map(event => tr.isUserEvent(event)).some(Boolean)) {
-          continue;
+  // local change를 브로드캐스트
+  const handleChange = useCallback(
+    (_: string, viewUpdate: ViewUpdate) => {
+      if (viewUpdate.docChanged) {
+        // eslint-disable-next-line no-restricted-syntax
+        for (const tr of viewUpdate.transactions) {
+          const events = ['select', 'input', 'delete', 'move', 'undo', 'redo'];
+          if (!events.map(event => tr.isUserEvent(event)).some(Boolean)) {
+            continue;
+          }
+          if (tr.annotation(Transaction.remote)) {
+            continue;
+          }
+          let adj = 0;
+          tr.changes.iterChanges((fromA, toA, _, __, inserted) => {
+            const insertText = inserted.toJSON().join('\n');
+            doc.update(root => {
+              root.content.edit(fromA + adj, toA + adj, insertText);
+            }, `update content byA ${client.current.getID()}`);
+            adj += insertText.length - (toA - fromA);
+          });
         }
-        if (tr.annotation(Transaction.remote)) {
-          continue;
-        }
-        let adj = 0;
-        tr.changes.iterChanges((fromA, toA, _, __, inserted) => {
-          const insertText = inserted.toJSON().join('\n');
-          doc.current.update(root => {
-            root.content.edit(fromA + adj, toA + adj, insertText);
-          }, `update content byA ${client.current.getID()}`);
-          adj += insertText.length - (toA - fromA);
-        });
       }
-    }
-  }, []);
+    },
+    [doc],
+  );
+
+  const syncText = () => {
+    const text = doc.getRoot().content;
+    codeMirrorView.current.view?.dispatch({
+      changes: {
+        from: 0,
+        to: codeMirrorView.current.view?.state.doc.length,
+        insert: text.toString(),
+      },
+      annotations: [Transaction.remote.of(true)],
+    });
+  };
+
+  useEffect(() => {
+    // 02-1. create a document then attach it into the client.
+    const attachDoc = async () => {
+      await client.current.activate();
+
+      await client.current.attach(doc);
+
+      doc.update(root => {
+        if (!root.content) {
+          // eslint-disable-next-line no-param-reassign
+          root.content = new yorkie.Text();
+        }
+      }, 'create content if not exists');
+
+      doc.subscribe(event => {
+        if (event.type === DocEventType.Snapshot) syncText();
+
+        const text = doc.getRoot().content;
+        console.log('#####', event, doc.getRoot());
+        console.log('text', text.toString());
+        setContent(text.toString());
+      });
+
+      doc.subscribe('$.content', event => {
+        if (event.type === DocEventType.RemoteChange) {
+          const { operations } = event.value;
+          handleOperations(operations);
+        }
+      });
+
+      await client.current.sync();
+      syncText();
+    };
+
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    attachDoc();
+  });
 
   // 코드 에디터의 최대 높이를 렌더링된 사이즈만큼 지정합니다.
   useEffect(() => {
@@ -108,38 +158,7 @@ const CodeEditor = () => {
     setMaxHeight(`${ref.current.offsetHeight - 10}px`);
   }, []);
 
-  useEffect(() => {
-    // 02-1. create a document then attach it into the client.
-    const attachDoc = async () => {
-      await client.current.activate();
-
-      await client.current.attach(doc.current);
-
-      doc.current.update(root => {
-        if (!root.content) {
-          // eslint-disable-next-line no-param-reassign
-          root.content = new yorkie.Text();
-        }
-      }, 'create content if not exists');
-
-      doc.current.subscribe(event => {
-        const text = doc.current.getRoot().content;
-        console.log('#####', event, doc.current.getRoot());
-        console.log('text', text.toString());
-        // /setContent((text.toString());
-      });
-
-      doc.current.subscribe('$.content', event => {
-        if (event.type === DocEventType.RemoteChange) {
-          const { operations } = event.value;
-          handleOperations(operations);
-        }
-      });
-    };
-
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    attachDoc();
-  });
+  // syncText();
 
   return (
     <Container ref={ref}>
@@ -149,6 +168,7 @@ const CodeEditor = () => {
         extensions={[java(), editorTheme]}
         onChange={handleChange}
         value={content}
+        ref={codeMirrorView}
       />
     </Container>
   );
