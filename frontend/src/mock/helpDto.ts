1a018fc087a842359b13ddd9264c482989677ac1
박진아 <dndbekfrl1@gmail.com>
2024-02-05 10:15:15 +0900

    Feat: 도움 요청하기 요청, 수락

---
+++ frontend/src/mock/helpDto.ts
@@ -0,0 +1,6 @@
+const helpDto = {
+  num: 1001,
+  title: '도와주세요2222',
+  content: '살려주세요333333ㅜㅜㅜㅜ',
+};
+export { helpDto };
---
+++ frontend/src/mock/index.ts
@@ -0,0 +1,5 @@
+// TODO: 실제 API 연결 후 삭제
+import { loginUserA, loginUserB } from './user';
+import { helpDto } from './helpDto';
+
+export { loginUserA, helpDto, loginUserB };
