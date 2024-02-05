57de1a5c95799a9604d2249786322e9525eaf724
차우열 <dtdtdz@gmail.com>
2024-02-05 17:26:03 +0900

    CodeHistory localdatetime S10P12A709-188

--- backend/coala/src/main/java/com/ssafy/coala/domain/help/application/MatchingServiceImpl.java
+++ backend/coala/src/main/java/com/ssafy/coala/domain/help/application/MatchingServiceImpl.java
@@ -27,7 +27,7 @@ public class MatchingServiceImpl implements MatchingService{
     private HelpAlarmRepository helpAlarmRepository;
 
 
-    //
+    //매칭 완료
     @Override
     public void notifyMatching(WaitDto waitDto) {
         if(redisService.isExist(waitDto)){
@@ -37,6 +37,7 @@ public class MatchingServiceImpl implements MatchingService{
             redisService.removeUser(waitDto);
 
             UUID roomUuid = waitDto.getRoomUuid();
+            //룸 생성 helpDto 정보
         }else{
             System.out.println("만료된 요청입니다.");
         }
