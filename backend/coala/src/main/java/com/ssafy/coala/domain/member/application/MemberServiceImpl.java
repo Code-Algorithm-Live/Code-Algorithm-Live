0f28f270071dbfd71505ec2262d7ca8ca2dd399e
차우열 <dtdtdz@gmail.com>
2024-02-08 12:22:02 +0900

    ProblemController.getProblem 더미 제거 S10P12A709-133

--- backend/coala/src/main/java/com/ssafy/coala/domain/member/application/MemberServiceImpl.java
+++ backend/coala/src/main/java/com/ssafy/coala/domain/member/application/MemberServiceImpl.java
@@ -7,13 +7,18 @@ import com.ssafy.coala.domain.member.dto.CustomUserDetails;
 import com.ssafy.coala.domain.member.domain.MemberProfile;
 import com.ssafy.coala.domain.member.dao.MemberProfileRepository;
 import com.ssafy.coala.domain.member.dto.MemberDto;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -26,9 +31,9 @@ public class MemberServiceImpl implements MemberService {
     private final MemberProfileRepository memberProfileRepository;
     private final MemberRepository memberRepository;
     private final RedisTemplate<String, String> redisStringTemplate;
-
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
+    @Value("${file.upload.directory}")
+    private String directoryPath;
 
     public MemberServiceImpl(MemberProfileRepository memberProfileRepository, MemberRepository memberRepository, RedisTemplate<String, String> redisStringTemplate, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.memberProfileRepository = memberProfileRepository;
@@ -118,5 +123,44 @@ public class MemberServiceImpl implements MemberService {
             result.add(memberDto);
         }
         return result;
+    }
+
+    @PostConstruct
+    private void mkdirs(){
+        File directory = new File(directoryPath +"images");
+        if (!directory.exists())
+            directory.mkdirs();
+    }
+
+    @Transactional
+    @Override
+    public boolean saveImage(String nickname, MultipartFile image) throws IOException {
+        String ImageUrl = memberProfileRepository.findImageUrlByNickname(nickname);
+        if(ImageUrl!=null){
+            File file = new File(ImageUrl);
+            if (file.exists()){
+                if (!file.delete()) return false;
+            }
+        }
+
+        String filePath = directoryPath+"images/"+nickname+getFileExtension(image.getOriginalFilename());
+        memberProfileRepository.updateImageUrlByNickname(filePath, nickname);
+        File dest = new File(filePath);
+        image.transferTo(dest);
+
+        return true;
+    }
+
+    public Byte[] getImageByte(String nickname){
+//        memberProfileRepository.findImageUrlByNickname(nickname)
+        return null;
+    }
+
+    private String getFileExtension(String fileName) {
+        int lastIndex = fileName.lastIndexOf('.');
+        if (lastIndex == -1) {
+            return ""; // 파일 확장자가 없는 경우
+        }
+        return fileName.substring(lastIndex);
     }
 }
