e1b7040306a016fecbb061ff23a5562b3e6ad5ff
dev-hamster <hamsterwave0925@gmail.com>
2024-02-11 23:53:40 +0900

    Feat: iframe 클릭 방지

--- frontend/src/components/Chat/ProblemView/index.tsx
+++ frontend/src/components/Chat/ProblemView/index.tsx
@@ -5,8 +5,13 @@ import { fetchProblemCrawl } from '@/api/chat';
 import styles from '@/components/Chat/ProblemView/style.module.scss';
 import useHelpFromStore from '@/store/helpForm';
 
+const disableClick = (document: Document) => {
+  document.body.addEventListener('click', e => e.preventDefault());
+};
+
 const ProblemView = () => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const iframeRef = useRef<HTMLIFrameElement>(null);
   const [eleOffset, setEleOffset] = useState<{
     width: number;
     height: number;
@@ -27,9 +32,27 @@ const ProblemView = () => {
     });
   }, []);
 
-  // if (isLoading) return <>로딩중입니다.</>;
+  useEffect(() => {
+    if (!problemSrc) return;
+
+    const iframe = iframeRef.current;
+    if (iframe && problemSrc) {
+      iframe.srcdoc = problemSrc.data as unknown as string;
+      iframe.onload = () => {
+        const contentDoc = iframe?.contentDocument;
+        if (contentDoc) {
+          disableClick(contentDoc);
+        }
+      };
+    }
+  }, [problemSrc]);
+
   return (
-    <div className={styles.container} ref={containerRef}>
+    <div
+      className={styles.container}
+      ref={containerRef}
+      style={{ position: 'relative' }}
+    >
       {isLoading && (
         <div
           style={{
@@ -41,11 +64,19 @@ const ProblemView = () => {
         </div>
       )}
       {!isLoading && (
-        <iframe
-          width={eleOffset?.width}
-          height={eleOffset?.height}
-          srcDoc={problemSrc?.data}
-        />
+        <div
+          style={{
+            width: `${eleOffset?.width}px`,
+            height: `${eleOffset?.height}px`,
+            overflow: 'scroll',
+          }}
+        >
+          <iframe
+            ref={iframeRef}
+            height={eleOffset?.height}
+            style={{ height: '100%', width: '100%' }}
+          />
+        </div>
       )}
     </div>
   );
