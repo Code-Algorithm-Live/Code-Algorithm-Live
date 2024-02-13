edd078e8478e188ca1c98f91b5a6530fc6cfb36c
이재종 <wowhd45@naver.com>
2024-02-13 12:56:59 +0900

    Feat: 미들웨어 S10P12A709-234

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -7,15 +7,25 @@ export async function middleware(req: NextRequest) {
   const session = await getToken({ req, secret, raw: true });
   const { pathname } = req.nextUrl;
 
-  // 로그인 페이지가 아닌데 세션이 없으면 로그인 페이지로
-  if (!pathname.startsWith('/login')) {
-    if (!session) {
-      return NextResponse.redirect(new URL('/login', req.url));
-    }
+  // 로그인 페이지나 회원가입 페이지가 아니면서 세션이 없는 경우
+  if (
+    !pathname.startsWith('/login') &&
+    !pathname.startsWith('/signup') &&
+    !session
+  ) {
+    return NextResponse.redirect(new URL('/login', req.url));
   }
+
   return undefined;
 }
 
 export const config = {
-  matcher: ['/login'],
+  matcher: [
+    '/',
+    '/help/:path*',
+    '/chat/:path*',
+    '/history/:path*',
+    '/login/:path*',
+    '/notice/:path*',
+  ],
 };
